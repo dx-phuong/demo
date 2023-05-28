5f2e0ba45b2994f03ab2931491512d5618b2bc79
dx-phuong <phuongdx128@gmail.com>
2023-05-28 22:52:18 +0700

    add

---
+++ main.js
@@ -0,0 +1,35 @@
+// var ElTitle = document.querySelector("main__left-item-title");
+// console.log(ElTitle);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// làm  1 bài toán với for và array function có sẵn trong js để so sánh
+
+
